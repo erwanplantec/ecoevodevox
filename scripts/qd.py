56d97462c4ec52f0ac2d56e22207c8949f8b6a41
Erwan Plantec <eplantec@hotmail.fr>
2025-03-25 09:32:32 +0100

    fix

--- scripts/qd.py
+++ scripts/qd.py
@@ -1,4 +1,5 @@
 from functools import partial
+from annotated_types import IsInfinite
 from jax.flatten_util import ravel_pytree
 from jaxtyping import PyTree
 import realax as rx
@@ -32,6 +33,7 @@ class Config(NamedTuple):
 	batch_size: int=256
 	grid_shape: tuple=(32,32)
 	plot_freq: int=100
+	make_animation: bool|str="ask"
 	# --- env ---
 	has_target: bool=False
 	maze: str="standard"
@@ -141,7 +143,7 @@ def train(cfg: Config, key: jax.Array):
 	)
 	def variation_fn(x1, x2, key):
 		x_varied, key = _variation_fn(x1, x2, key)
-		x_varied = jnp.where(prms_mask.astype(bool), x_varied, x1) #type:ignore
+		x_varied = jnp.where(prms_mask.astype(bool), x_varied, x2) #type:ignore
 		return x_varied, key
 
 	emitter = MixingEmitter(mutation_fn, variation_fn, cfg.variation_percentage, cfg.batch_size) #type:ignore
@@ -214,31 +216,59 @@ def train(cfg: Config, key: jax.Array):
 	state = jax.block_until_ready(trainer.train_(init_state, key_train))
 
 	fig, ax = plt.subplots(1, 3, figsize=(18,6), sharey=True)
-	cam = Camera(fig)
-	max_fitness = max([r.fitnesses.max() for r in repertoires])
-	min_fitness = min([np.where(np.isinf(r.fitnesses), np.inf, r.fitnesses).min() for r in repertoires])
-	max_types = max([np.max(prms_shaper.reshape(r.genotypes).types.active.sum(-1)) for r in repertoires])
-	min_types = 0
-	for repertoire in repertoires:
-		fitnesses = repertoire.fitnesses
-		genotypes = repertoire.genotypes
-		mask = ~np.isinf(fitnesses)
-		prms: PyTree = prms_shaper.reshape(genotypes)
-		active_types = prms.types.active.sum(-1)
-		network_size = np.sum(prms.types.active * prms.types.pi * cfg.N_gain, axis=-1)
+	repertoire = state.repertoire
+	fitnesses = repertoire.fitnesses
+	genotypes = repertoire.genotypes
+	mask = ~np.isinf(fitnesses)
+	prms: PyTree = prms_shaper.reshape(genotypes)
+	active_types = prms.types.active.sum(-1)
+	network_size = np.sum(prms.types.active * prms.types.pi * cfg.N_gain, axis=-1)
 
-		plot_2d_map_elites_repertoire(trainer.centroids, fitnesses, minval=0.0, maxval=1.0, ax=ax[0], vmin=min_fitness, vmax=max_fitness) #type:ignore
-		ax[0].set_title("fitness")
-		plot_2d_map_elites_repertoire(trainer.centroids, np.where(mask, active_types, -jnp.inf), minval=0.0, maxval=1.0, ax=ax[1], vmin=min_types, vmax=max_types)
-		ax[1].set_title("#types")
-		ax[1].set_ylabel("")
-		plot_2d_map_elites_repertoire(trainer.centroids, np.where(mask, network_size, -jnp.inf), minval=0.0, maxval=1.0, ax=ax[2])
-		ax[2].set_title("N")
-		ax[2].set_ylabel("")
+	plot_2d_map_elites_repertoire(trainer.centroids, fitnesses, minval=0.0, maxval=1.0, ax=ax[0]) #type:ignore
+	ax[0].set_title("fitness")
+	plot_2d_map_elites_repertoire(trainer.centroids, np.where(mask, active_types, -jnp.inf), minval=0.0, maxval=1.0, ax=ax[1])
+	ax[1].set_title("#types")
+	ax[1].set_ylabel("")
+	plot_2d_map_elites_repertoire(trainer.centroids, np.where(mask, network_size, -jnp.inf), minval=0.0, maxval=1.0, ax=ax[2])
+	ax[2].set_title("N")
+	ax[2].set_ylabel("")
+	fig.tight_layout()
+	wandb.log(dict(final_result=wandb.Image(fig)))
 
-		cam.snap()
-	ani = cam.animate()
-	wandb.log({"result": wandb.Html(ani.to_html5_video())})
+	#-------------------------------------------------------------------
+	if cfg.make_animation=="ask":
+		make_animation=input("make animation ?")
+		make_animation = make_animation in ["y", "Y"]
+	else:
+		make_animation = cfg.make_animation
+
+	if make_animation:
+		fig, ax = plt.subplots(1, 3, figsize=(18,6), sharey=True)
+		cam = Camera(fig)
+		max_fitness = max([r.fitnesses.max() for r in repertoires])
+		min_fitness = min([np.where(np.isinf(r.fitnesses), np.inf, r.fitnesses).min() for r in repertoires])
+		max_types = max([np.max(prms_shaper.reshape(r.genotypes).types.active.sum(-1)) for r in repertoires])
+		min_types = 0
+		for repertoire in repertoires:
+			fitnesses = repertoire.fitnesses
+			genotypes = repertoire.genotypes
+			mask = ~np.isinf(fitnesses)
+			prms: PyTree = prms_shaper.reshape(genotypes)
+			active_types = prms.types.active.sum(-1)
+			network_size = np.sum(prms.types.active * prms.types.pi * cfg.N_gain, axis=-1)
+
+			plot_2d_map_elites_repertoire(trainer.centroids, fitnesses, minval=0.0, maxval=1.0, ax=ax[0], vmin=min_fitness, vmax=max_fitness) #type:ignore
+			ax[0].set_title("fitness")
+			plot_2d_map_elites_repertoire(trainer.centroids, np.where(mask, active_types, -jnp.inf), minval=0.0, maxval=1.0, ax=ax[1], vmin=min_types, vmax=max_types)
+			ax[1].set_title("#types")
+			ax[1].set_ylabel("")
+			plot_2d_map_elites_repertoire(trainer.centroids, np.where(mask, network_size, -jnp.inf), minval=0.0, maxval=1.0, ax=ax[2])
+			ax[2].set_title("N")
+			ax[2].set_ylabel("")
+
+			cam.snap()
+		ani = cam.animate()
+		wandb.log({"result": wandb.Html(ani.to_html5_video())})
 
 	if cfg.log: wandb.finish()
 
