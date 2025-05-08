2e7d22c3818058f54ab53b1e907c5af578387214
Erwan Plantec <eplantec@hotmail.fr>
2025-05-08 03:37:26 +0200

    fix

--- scripts/sim.py
+++ scripts/sim.py
@@ -139,7 +139,7 @@ def metrics_fn(state: EnvState, step_data: dict)->dict:
 		"population": agents.alive.sum(),
 		"energy_levels": agents.energy,
 		"offsprings": agents.n_offsprings,
-		**step_data,
+		"energy_intakes": step_data["energy_intakes"],
 		"ages": agents.age,
 		**food_levels,
 		"total_food": state.food.sum(),
@@ -150,13 +150,16 @@ def metrics_fn(state: EnvState, step_data: dict)->dict:
 def host_log_transform(data: dict)->dict:
 	data = jax.tree.map(np.asarray, data)
 	mask = data["alive"]
+	avg_data = {}
 	for k, v in data.items():
 		if k=="alive" or not v.shape:
 			continue
 		if v.shape[0]==mask.shape[0]:
 			data[k] = v[mask]
+			avg_data[f"{k} (avg)"] = data[k]
 	data["reproduction_rates"] = data["offsprings"] / data["ages"]
 	del data["alive"] 
+	data = {**data, **avg_data}
 	return data
 
 
